a3d64834205626b04fed8f7eb3e4607ef2df21ec
PaulHank <Pablo585.PAE@gmail.com>
2022-11-10 18:05:25 +0100

    Segundo commit, añadida lógica del menu

---
+++ Ej1PAE/app/src/main/java/com/liceolapaz/dam/pae/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.liceolapaz.dam.pae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var error : TextView
+    private lateinit var login : Button
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        error = findViewById(R.id.errorDisp)
+        login = findViewById(R.id.login)
+        password = findViewById(R.id.password)
+        username = findViewById(R.id.Username)
+
+
+
+        login.setOnClickListener {
+            var numIntentos = 3;
+
+            if (!username.equals("admin")) {
+                error.text = "El nombre de usuario es incorrecto"
+                error.error
+                error.visibility = VISIBLE
+                if (!password.equals("liceo")) {
+                    error.text = "La contraseña es incorrecta"
+                    error.error
+                    error.visibility = VISIBLE
+                    numIntentos--
+                }
+                numIntentos--
+            }
+
+            if (numIntentos == 0) {
+                exitProcess(0)
+            }
+
+
+            startActivity(intent)
+        }
+    }
+}
